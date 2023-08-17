a38bc49cabb99dbb84eac39dce3db387b9e3d9a6
Thanarat420 <taexxx4@gmal.com>
2023-08-17 10:01:55 +0700

    hw4-5

--- Homework/HW04/hw04-5-time-convert.cpp
+++ Homework/HW04/hw04-5-time-convert.cpp
@@ -13,3 +13,34 @@
     Output:
         27 days = 2332800 seconds
 */
+#include <stdio.h>
+
+int main() 
+{
+
+//กำหนดค่าตัวเเปร
+
+float day , hour , min , sec ;
+
+//รับค่าจำนวนวัน
+
+printf ( "Input Days : " ) ;
+scanf ( "%f" , &day ) ;
+
+hour = 24 ;
+min = 60 ;
+
+//คำนวนเเปลงหน่วย
+
+sec = ( day * hour * min ) * 60 ;
+
+
+//เเสดงค่าการเเปลงหน่วย
+
+printf ( "%.0f days = %.0f seconds", day , sec ) ;
+
+return 0 ;
+
+
+
+}
